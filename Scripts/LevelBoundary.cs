7e8f7845f24e62e336b9bf002c58bdab6548c3a8
Jack Livingston <jhliving@live.unc.edu>
2015-04-14 23:55:24 -0400

    Created a mechanism that allows you to return to a checkpoint after falling off the map.  Coordinates of checkpoints submitted manually

---
+++ Scripts/LevelBoundary.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelBoundary : MonoBehaviour {
+	
+	private GameObject playerObject;
+	
+	// Use this for initialization
+	void Start () {
+		playerObject = GameObject.FindGameObjectWithTag("Player");
+		//maincontroller = playerObject.GetComponent<MainGUI>("MainGUI");//GameObject.FindGameObjectWithTag("Player").GetComponent("MainGUI");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log("This Transform Position: "+transform.position);
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			Debug.Log("Player Dead");
+			playerObject.SendMessage("PlayDeath");
+			playerObject.SendMessage("RevertToCheckpoint");
+		}
+	}
+}
