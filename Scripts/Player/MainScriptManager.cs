fd3314dd350e013338058a9309a1988b682448a7
Jack Livingston <jhliving@live.unc.edu>
2015-04-29 17:02:28 -0400

    Added MainScriptManager that I incorrectly didn't put in the Assets folder previously

---
+++ Scripts/Player/MainScriptManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainScriptManager : MonoBehaviour {
+	
+	
+	public static int sceneProgression = 3;
+	public static bool  isPause = false;
+	
+	public void  ResetGame (){
+		
+	}
+}
