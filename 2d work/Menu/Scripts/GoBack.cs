19336979d08de11a13f33ed776538f3597f4f167
blam627 <blam.live.unc.edu>
2015-04-28 20:18:47 -0400

    Created Menu

---
+++ 2d work/Menu/Scripts/GoBack.cs
@@ -0,0 +1,21 @@
+﻿//Main Menu
+//Attached to main camera
+
+using UnityEngine;
+using System.Collections;
+
+public class GoBack : MonoBehaviour {
+
+	
+	public float guiPlacementX1;
+	public float guiPlacementY1;
+	
+	void OnGUI(){
+		
+		//Display our buttons
+		if (GUI.Button (new Rect (Screen.width * guiPlacementX1, Screen.height * guiPlacementY1, Screen.width * .5f, Screen.height * .1f), "Back to Main")) {  
+			Application.LoadLevel("MainMenu");
+		}
+
+	}
+}
