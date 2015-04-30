918877231868d9b63990cd9756a23652ab56aad7
Jack Livingston <jhliving@live.unc.edu>
2015-04-30 13:25:47 -0400

    Created timeout to return to main menu after beating the game

---
+++ Scripts/Environment/TimeoutToMainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeoutToMainMenu : MonoBehaviour {
+
+	// Use this for initialization
+
+	public float timeout;
+	void Start () {
+		timeout = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeout > 5) {
+			Application.LoadLevel("MainMenu");
+		} else {
+			timeout = timeout + Time.deltaTime;
+		}
+	}
+}
